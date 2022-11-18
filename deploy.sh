9c65e7858722e69ec89f6b4798adf5f3349745de
JoseShimoguiri <zekshimoguiri@gmail.com>
2022-11-18 01:19:44 +0000

    trabalho

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+
+kubectl apply -f kubernetes-manifests/adservice.yaml
+kubectl apply -f kubernetes-manifests/cartservice.yaml
+kubectl apply -f kubernetes-manifests/checkoutservice.yaml
+kubectl apply -f kubernetes-manifests/currencyservice.yaml
+kubectl apply -f kubernetes-manifests/frontend.yaml
+kubectl apply -f kubernetes-manifests/kustomization.yaml
+kubectl apply -f kubernetes-manifests/loadgenerator.yaml
+kubectl apply -f kubernetes-manifests/paymentservice.yaml
+kubectl apply -f kubernetes-manifests/productcatalogservice.yaml
+kubectl apply -f kubernetes-manifests/recommendationservice.yaml
+kubectl apply -f kubernetes-manifests/shippingservice.yaml
+kubectl apply -f kubernetes-manifests/emailservice.yaml
+kubectl apply -f kubernetes-manifests/redis.yaml
